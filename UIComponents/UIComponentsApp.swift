dfda640744b3416ca53f5037fb69ab836fda8ce8
Ernesto Cisnero <miguelernestocisnero@gmail.com>
2026-02-22 17:42:05 -0500

    Initial Commit

---
+++ UIComponents/UIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIComponentsApp.swift
+//  UIComponents
+//
+//  Created by Ernesto Cisnero on 2/22/26.
+//
+
+import SwiftUI
+
+@main
+struct UIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
